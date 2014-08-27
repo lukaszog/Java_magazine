76d2def3ea111b2496d80052ca003c4fc2f4205a
Łukasz Ogan <lukasz.ogan@gmail.com>
2014-08-27 19:29:05 +0200

    Add files

--- src/com/magazyn/view/View.java
+++ src/com/magazyn/view/View.java
@@ -1,41 +1,6 @@
 package com.magazyn.view;
 
-import java.awt.Checkbox;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.HeadlessException;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.print.PrinterException;
-
-import javax.swing.DefaultCellEditor;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-
-import com.magazyn.controller.Controller;
-import com.magazyn.model.Client;
-import com.magazyn.model.Company;
-import com.magazyn.model.Database;
-import com.magazyn.model.Item;
-import com.magazyn.model.Model;
-import com.magazyn.model.Category;
-import com.magazyn.model.Order;
-
-import java.awt.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.magazyn.model.*;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -44,10 +9,13 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.print.PrinterException;
+import java.util.List;
 
 public class View extends JFrame implements ActionListener, CategoryListener,
 		CompanyListener, ClientsListener, ItemsListener, OrderListener {
